6fc4b158581fd4bc363f14883443da9f1e227ffb
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-06-24 12:29:12 +0100

    configure redis connection

---
+++ src/config/redis_config.py
@@ -0,0 +1,10 @@
+import os
+from redis_om import get_redis_connection
+
+
+redis_db = get_redis_connection(
+    host=os.getenv("REDIS_HOST"),
+    port=int(os.getenv("REDIS_PORT")),
+    password=os.getenv("REDIS_PASSWORD"),
+    decode_responses=True,
+)
